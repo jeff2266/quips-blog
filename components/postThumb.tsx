e0d7ff76b596b834dee2f7d84503e440815947b3
jyan <jeff2266@gmail.com>
2024-10-24 20:19:49 -0700

    get posts from nhost

--- components/postThumb.tsx
+++ components/postThumb.tsx
@@ -1,10 +1,46 @@
+'use client'
+
+import { useNhostClient } from '@nhost/nextjs'
+import { useCallback, useEffect, useState } from 'react'
+
+const qGetPrompts = `
+    query {
+        prompt {
+            id
+            prompt
+            user {
+                displayName
+            }
+            created
+        }
+    }
+`
+
 export default function PostThumb() {
-    return <div className="border">
-        <h3 className="text-lg">Prompt</h3>
-        <div className="flex">
-            <div className="text-xs">Likes</div>
-            <div className="text-xs">Author</div>
-            <div className="text-xs">Age</div>
-        </div>
-    </div>
+	const nhostClient = useNhostClient()
+	const [prompts, setPrompts] = useState<any>(null)
+	const getPromptsCallback = useCallback<() => Promise<void>>(async () => {
+		const data = await nhostClient.graphql.request(qGetPrompts)
+        console.log(data)
+		setPrompts(data)
+	}, [])
+
+	useEffect(() => {
+		getPromptsCallback().catch(e => console.error(e))
+	}, [getPromptsCallback])
+
+	return (
+		<div className="border">
+			{prompts?.data?.prompt?.map((p: any) => (
+				<div key={p?.id}>
+					<h3 className="text-lg">{p?.prompt}</h3>
+					<div className="flex">
+						<div className="text-xs">Likes</div>
+						<div className="text-xs">{p?.user?.displayName}</div>
+						<div className="text-xs">{p?.created}</div>
+					</div>
+				</div>
+			))}
+		</div>
+	)
 }
